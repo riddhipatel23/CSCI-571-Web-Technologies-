79666787ec21bdd6cba0f86dd8663a4f6a627388
Riddhi Patel <riddhiha@usc.edu>
2022-04-06 02:36:35 -0700

    changed title of App

--- CS571-HW3-Arc/arc/src/App.js
+++ CS571-HW3-Arc/arc/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import * as ReactDOM from 'react-dom';
 import { Scene,WebMap,WebScene } from '@esri/react-arcgis';
 
 
 function App(){
+  useEffect(() => {
+    document.title = "React App - Riddhi Patel"
+  }, [])
   ReactDOM.render(
     <div style={{ width: '100vw', height: '100vh' }}>
         <WebMap id="c3a272c7050f4429b7d1421d65c39b75" />
